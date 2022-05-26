306b23a0c64d653065744082ce97f9d896465039
Alfredo Deza <alfredo@deza.pe>
2022-05-25 20:13:27 -0400

    update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,6 +1,11 @@
+import os
 
 def main():
-  print("Hellow from GitHub Actions!")
+  print("Hello from GitHub Actions!")
+  token = os.environ.get("AZURE_SECRET_TOKEN")
+  if not token:
+    raise RuntimeError("AZURE_SECRET_TOKEN env var is not set!")
+  print("All good! we found our env var")
   
 
 if __name__ == '__main__':
